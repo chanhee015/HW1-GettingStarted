4e12e2b83296a70f9e939873fa42a01687cf7b04
Sang Kil Cha <sangkilc@kaist.ac.kr>
2019-02-27 19:39:35 +0900

    Add HW1

--- HW1.tests/Tests.fs
+++ HW1.tests/Tests.fs
@@ -6,6 +6,33 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 [<TestClass>]
 type TestClass () =
 
+  let filename = "myinfo.csv"
+  let lines = IO.File.ReadLines filename |> Seq.toArray
+
   [<TestMethod>]
-  member this.TestMethodPassing () =
-    Assert.IsTrue(true);
+  member __.``Does your CSV file have just two lines?``() =
+    Assert.AreEqual (2, Array.length lines)
+
+  [<TestMethod>]
+  member __.``Does your CSV file has the correct header?``() =
+    let expected = "student_id,email,github_id"
+    Assert.AreEqual (expected, lines.[0])
+
+  [<TestMethod>]
+  member __.``Does your CSV file contain a valid student ID?``() =
+    let studentID = lines.[1].Split(',').[0] |> Convert.ToUInt64
+    Assert.IsTrue (studentID > 0UL)
+
+  [<TestMethod>]
+  member __.``Does your CSV file contain a valid email?``() =
+    let email = lines.[1].Split(',').[1]
+    let emailID = email.Split('@').[0]
+    let emailDomain = email.Split('@').[1]
+    Assert.IsTrue (email.Contains ("@"))
+    Assert.IsTrue (String.length emailID > 0)
+    Assert.IsTrue (String.length emailDomain > 0)
+
+  [<TestMethod>]
+  member __.``Does your CSV file contain a valid github ID?``() =
+    let gitid = lines.[1].Split(',').[2]
+    Assert.IsTrue (String.length gitid > 0)
---
+++ README.md
@@ -0,0 +1,27 @@
+# HW1
+
+Modify the `myinfo.csv` file in such a way that satisfies the following
+conditions:
+1. The very first line should have the exact string (no extra space chars
+   allows):
+
+    ```
+    student_id,email,github_id
+    ```
+
+2. The next (2nd) line should contain a comma-separated values for your student
+   ID, email address, and GitHub ID. For example, suppose your student ID, email
+   address, and GitHub ID are "20001234", "myemail@kaist.ac.kr", and "awesomeid",
+   respectively. Then the 2nd line of the `myinfo.csv` file should be:
+
+    ```
+    20001234,myemail@kaist.ac.kr,awesomeid
+    ```
+
+3. In conclusion, the `myinfo.csv` file should have two lines in total, and it
+   should look as below:
+
+    ```
+    student_id,email,github_id
+    20001234,myemail@kaist.ac.kr,awesomeid
+    ```
